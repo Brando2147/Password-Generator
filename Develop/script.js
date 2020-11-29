207b7f1a80590daba6fe4166716777fc4ca94cf9
Brando D <brando2147@gmail.com>
2020-11-29 13:22:25 -0700

    Adding validation to password

--- Develop/script.js
+++ Develop/script.js
@@ -14,30 +14,37 @@ function writePassword() {
 
 // Function to generate password
 function generatePassword() {
+  let availChar = [];
+  let finalChar = [];
+  let generatedPassword = '';
 
 
+  // User input  
   let userlength = prompt('Please choose your password length (8-128)');
   let useSpec = confirm('Include special characters?');
   let useLower = confirm('Include lowercase characters?');
   let useUpper = confirm('Include uppercase characters?');
   let useNum = confirm('Include numeric characters?');
-  let generatedPassword = '';
 
 
+  // User prompt validation
+  if (userlength < 8) {
+    alert('Please make sure your password length is between 8-128')
+  }
 
+  if (!useSpec && !useLower && !useUpper && !useNum) {
+    alert('Please choose at least one character type.')
+  }
+
+
+  // Available characters 
   let specArr = ['!', '#', '$', '%', '&', '\\', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '^', '[', '', ']', '^', '_', '`', '{', '|', '}', '~']
   let lowerArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
   let upperArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   let numArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-  let availChar = [];
-  let finalChar = [];
 
-  // console.log(userlength);
-  // if (userlength <= 8) {
-  //   alert('Please choose a number 8-128');
-  // }
-
+  // Logic to add chosen characters from Array
   if (useSpec) {
     availChar = availChar.concat(specArr);
   }
@@ -55,7 +62,7 @@ function generatePassword() {
   }
 
 
-
+  //Iterates the user length and chooses random characters from Arrays
   for (i = 0; i <= userlength; i++) {
     let i = Math.floor(Math.random() * availChar.length);
     generatedPassword += availChar[i];
@@ -63,15 +70,16 @@ function generatePassword() {
 
 
   }
+
+  // for (index = 0; index < finalChar.length; index++) {
+
+  // }
+
   return generatedPassword;
 
 
 
 }
-
-
-// Alert the password has been generated
-// alert("Your password has been generated!")
 
 
 // Add event listener to generate button
