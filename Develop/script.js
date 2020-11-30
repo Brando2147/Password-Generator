c784adfbdaca31dcdfd79b214d4399ebd8fbbbc1
Brando D <brando2147@gmail.com>
2020-11-30 08:31:42 -0700

    Updated validation alerts

--- Develop/script.js
+++ Develop/script.js
@@ -19,21 +19,25 @@ function generatePassword() {
   let generatedPassword = '';
 
 
-  // User input  
-  let userlength = prompt('Please choose your password length (8-128)');
+  // Prompt user for password length
+  let userLength = prompt('Please choose your password length (8-128)');
+
+  // Validate user password length 
+  if (userLength < 8 || userLength > 128) {
+    alert('Please make sure your password length is between (8-128)');
+    return '';
+  }
+
+  // Prompt user for character selection
   let useSpec = confirm('Include special characters?');
   let useLower = confirm('Include lowercase characters?');
   let useUpper = confirm('Include uppercase characters?');
   let useNum = confirm('Include numeric characters?');
 
-
-  // User prompt validation
-  if (userlength < 8) {
-    alert('Please make sure your password length is between 8-128')
-  }
-
+  // Validate users selection 
   if (!useSpec && !useLower && !useUpper && !useNum) {
-    alert('Please choose at least one character type.')
+    alert('Please choose at least one character type.');
+    return '';
   }
 
 
@@ -63,17 +67,13 @@ function generatePassword() {
 
 
   //Iterates the user length and chooses random characters from Arrays
-  for (i = 0; i <= userlength; i++) {
+  for (i = 0; i < userLength; i++) {
     let i = Math.floor(Math.random() * availChar.length);
     generatedPassword += availChar[i];
-
-
-
   }
 
-  // for (index = 0; index < finalChar.length; index++) {
+  // Iterates through the generated password length and assigns at least one character from each array chosen
 
-  // }
 
   return generatedPassword;
 
